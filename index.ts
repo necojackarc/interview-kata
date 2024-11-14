ec5471d83f581e89ad5a5aad3da77ec613e04ee1
necojackarc <necojackarc@gmail.com>
2024-11-14 16:26:24 +0000

    Format file for consistency

--- index.ts
+++ index.ts
@@ -1,115 +1,116 @@
 'use strict';
 
 export interface Position {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
 export interface Plateau {
-  maxX: number;
-  maxY: number;
+    maxX: number;
+    maxY: number;
 }
 
 export type Direction = 'N' | 'E' | 'S' | 'W';
+
 export type Command = 'M' | 'L' | 'R';
 
 export class Rover {
-  public position: Position;
-  public direction: Direction;
-  private plateau: Plateau;
+    public position: Position;
+    public direction: Direction;
+    private plateau: Plateau;
 
-  private directions: Direction[] = ['N', 'E', 'S', 'W'];
+    // Used to determine the next direction internally
+    private directions: Direction[] = ['N', 'E', 'S', 'W'];
 
-  constructor(position: Position, direction: Direction, plateau: Plateau) {
-    this.position = { ...position };
-    this.direction = direction;
-    this.plateau = plateau;
+    constructor(position: Position, direction: Direction, plateau: Plateau) {
+        this.position = { ...position }; // Clone an object to avoid side effect on the original object
+        this.direction = direction;
+        this.plateau = plateau;
   }
 
-  move(command: Command) {
-    if (command === 'L') {
-      this.direction = this.turnToLeft();
-    } else if (command === 'R') {
-      this.direction = this.turnToRight();
-    } else if (command === 'M') {
-      this.position = this.moveForward();
-    } else {
-      throw new Error(`Unexpected command ${command} is provided`);
+    move(command: Command) {
+        if (command === 'L') {
+            this.direction = this.turnToLeft();
+        } else if (command === 'R') {
+            this.direction = this.turnToRight();
+        } else if (command === 'M') {
+            this.position = this.moveForward();
+        } else {
+            throw new Error(`Unexpected command ${command} is provided`);
+        }
     }
 
-  }
-
-  private turnToRight(): Direction {
-    const currentDirectionIndex = this.directions.indexOf(this.direction);
-    const nextDirectionIndex = (this.directions.length + currentDirectionIndex + 1) % this.directions.length;
-    return this.directions[nextDirectionIndex]
-  }
-
-  private turnToLeft(): Direction {
-    const currentDirectionIndex = this.directions.indexOf(this.direction);
-    const nextDirectionIndex = (this.directions.length + currentDirectionIndex - 1) % this.directions.length;
-    return this.directions[nextDirectionIndex]
-  }
-
-  private moveForward(): Position {
-    const currentPosition = { ...this.position };
-
-    if (this.direction === 'N') {
-      if (currentPosition.y < this.plateau.maxY) {
-        currentPosition.y += 1;
-      }
-    } else if (this.direction === 'E') {
-      if (currentPosition.x < this.plateau.maxX) {
-        currentPosition.x += 1;
-      }
-    } else if (this.direction === 'S') {
-      if (currentPosition.y > 0) {
-        currentPosition.y -= 1;
-      }
-    } else if (this.direction === 'W') {
-      if (currentPosition.x > 0) {
-        currentPosition.x -= 1;
-      }
-    } else {
-      throw new Error('Rover is facing to the unexpected direction');
+    private turnToRight(): Direction {
+        const currentDirectionIndex = this.directions.indexOf(this.direction);
+        const nextDirectionIndex = (this.directions.length + currentDirectionIndex + 1) % this.directions.length;
+        return this.directions[nextDirectionIndex]
     }
 
-    return currentPosition;
-  }
+    private turnToLeft(): Direction {
+        const currentDirectionIndex = this.directions.indexOf(this.direction);
+        const nextDirectionIndex = (this.directions.length + currentDirectionIndex - 1) % this.directions.length;
+        return this.directions[nextDirectionIndex]
+    }
+
+    private moveForward(): Position {
+        const currentPosition = { ...this.position };
+
+        if (this.direction === 'N') {
+            if (currentPosition.y < this.plateau.maxY) {
+                currentPosition.y += 1;
+            }
+        } else if (this.direction === 'E') {
+            if (currentPosition.x < this.plateau.maxX) {
+                currentPosition.x += 1;
+            }
+        } else if (this.direction === 'S') {
+            if (currentPosition.y > 0) {
+                currentPosition.y -= 1;
+            }
+        } else if (this.direction === 'W') {
+            if (currentPosition.x > 0) {
+                currentPosition.x -= 1;
+            }
+        } else {
+            throw new Error('Rover is facing to the unexpected direction');
+        }
+
+        return currentPosition;
+    }
 }
 
 export function main(input: string[]): string[] {
-  const rawPlateau = input[0].split(' ');
+    const rawPlateau = input[0].split(' ');
 
-  const plateau = {
-    maxX: Number(rawPlateau[0]),
-    maxY: Number(rawPlateau[1]),
-  } as Plateau;
+    const plateau = {
+        maxX: Number(rawPlateau[0]),
+        maxY: Number(rawPlateau[1]),
+    } as Plateau;
 
-  const rovers: Rover[] = [];
+    const rovers: Rover[] = [];
 
-  for(let i = 1; i < input.length; i += 2) {
-    const rawPositionAndDirection = input[i].split(' ');
+    for(let i = 1; i < input.length; i += 2) {
+        const rawPositionAndDirection = input[i].split(' ');
 
-    const position = {
-      x: Number(rawPositionAndDirection[0]),
-      y: Number(rawPositionAndDirection[1]),
-    } as Position;
-    const direction = rawPositionAndDirection[2] as Direction;
+        const position = {
+            x: Number(rawPositionAndDirection[0]),
+            y: Number(rawPositionAndDirection[1]),
+        } as Position;
+        const direction = rawPositionAndDirection[2] as Direction;
 
-    const commands = input[i + 1].split('') as Command[];
+        const commands = input[i + 1].split('') as Command[];
 
-    const rover = new Rover(
-      position,
-      direction,
-      plateau
-    );
+        const rover = new Rover(
+            position,
+            direction,
+            plateau
+        );
 
-    commands.forEach((command) => {
-      rover.move(command);
-    })
+      commands.forEach((command) => {
+          rover.move(command);
+      })
 
-    rovers.push(rover);
+      rovers.push(rover);
   }
 
   return rovers.map((rover) => (`${rover.position.x} ${rover.position.y} ${rover.direction}`));
