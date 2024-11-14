3294cbf8079f5cff55f2845bf9b75313d2cd2461
necojackarc <necojackarc@gmail.com>
2024-11-14 15:49:46 +0000

    Tidy up test cases for 'M' command (test case refacotring)

--- index.test.ts
+++ index.test.ts
@@ -19,150 +19,141 @@ describe('Rover', () => {
         expect(rover.direction).toEqual(direction);
     });
 
-    describe('When Rover faces to the north', () => {
-        beforeEach(() => {
-            rover.direction = 'N';
+    describe('#move', () => {
+        describe('When Rover faces to the north', () => {
+            beforeEach(() => {
+                rover.direction = 'N';
+            });
+
+            test("Rover faces to the west after it turns left", () => {
+                const command: Command = 'L';
+                rover.move(command);
+                expect(rover.direction).toEqual('W');
+            })
+
+            test("Rover faces to the east after it turns right", () => {
+                const command: Command = 'R';
+                rover.move(command);
+                expect(rover.direction).toEqual('E');
+            })
+
+            test("Rover moves up when it moves forward", () => {
+                const command: Command = 'M';
+                rover.move(command);
+                expect(rover.position).toEqual({ x: 1, y: 3 });
+            })
         });
 
-        test("Rover faces to the west after it turns left", () => {
-            const command: Command = 'L';
-            rover.move(command);
-            expect(rover.direction).toEqual('W');
-        })
+        describe('When Rover faces to the west', () => {
+            beforeEach(() => {
+                rover.direction = 'W';
+            });
 
-        test("Rover faces to the east after it turns right", () => {
-            const command: Command = 'R';
-            rover.move(command);
-            expect(rover.direction).toEqual('E');
-        })
-    });
+            test("Rover faces to the south after it turns left", () => {
+                const command: Command = 'L';
+                rover.move(command);
+                expect(rover.direction).toEqual('S');
+            })
 
-    describe('When Rover faces to the west', () => {
-        beforeEach(() => {
-            rover.direction = 'W';
+            test("Rover faces to the north after it turns right", () => {
+                const command: Command = 'R';
+                rover.move(command);
+                expect(rover.direction).toEqual('N');
+            })
+
+            test("Rover moves left when it moves forward", () => {
+                const command: Command = 'M';
+                rover.move(command);
+                expect(rover.position).toEqual({ x: 0, y: 2 });
+            })
         });
 
-        test("Rover faces to the south after it turns left", () => {
-            const command: Command = 'L';
-            rover.move(command);
+        describe('When Rover faces to the south', () => {
+            beforeEach(() => {
+                rover.direction = 'S';
+            });
+
+            test("Rover faces to the east after it turns left", () => {
+                const command: Command = 'L';
+                rover.move(command);
+                expect(rover.direction).toEqual('E');
+            })
+
+            test("Rover faces to the west after it turns right", () => {
+                const command: Command = 'R';
+                rover.move(command);
+                expect(rover.direction).toEqual('W');
+            })
+
+            test("Rover moves down when it moves forwards", () => {
+                const command: Command = 'M';
+                rover.move(command);
+                expect(rover.position).toEqual({ x: 1, y: 1 });
+            })
+        });
+
+        describe('When Rover faces to the east', () => {
+            beforeEach(() => {
+                rover.direction = 'E';
+            });
+
+            test("Rover faces to the north after it turns left", () => {
+                const command: Command = 'L';
+                rover.move(command);
+                expect(rover.direction).toEqual('N');
+            })
+
+            test("Rover faces to the south after it turns right", () => {
+                const command: Command = 'R';
+                rover.move(command);
+                expect(rover.direction).toEqual('S');
+            })
+
+            test("Rover moves right when it moves forward", () => {
+                const command: Command = 'M';
+                rover.move(command);
+                expect(rover.position).toEqual({ x: 2, y: 2 });
+            })
+        });
+
+        test("Rover does not move beyond the upper edge of the plateau", () => {
+            for (let i = 0; i < plateau.maxY + 1; i++) {
+                rover.move('M');
+            }
+            expect(rover.position).toEqual({ x: 1, y: 5 });
+        })
+
+        test("Rover does not move beyond the bottom edge of the plateau", () => {
+            rover.move('R');
+            rover.move('R');
             expect(rover.direction).toEqual('S');
-        })
 
-        test("Rover faces to the north after it turns right", () => {
-            const command: Command = 'R';
-            rover.move(command);
-            expect(rover.direction).toEqual('N');
-        })
-    });
-
-    describe('When Rover faces to the south', () => {
-        beforeEach(() => {
-            rover.direction = 'S';
+            for (let i = 0; i < plateau.maxY + 1; i++) {
+                rover.move('M');
+            }
+            expect(rover.position).toEqual({ x: 1, y: 0 });
         });
 
-        test("Rover faces to the east after it turns left", () => {
-            const command: Command = 'L';
-            rover.move(command);
+        test("Rover does not move beyond the right edge of the plateau", () => {
+            rover.move('R');
             expect(rover.direction).toEqual('E');
+
+            for (let i = 0; i < plateau.maxX + 1; i++) {
+                rover.move('M');
+            }
+            expect(rover.position).toEqual({ x: 5, y: 2 });
         })
 
-        test("Rover faces to the west after it turns right", () => {
-            const command: Command = 'R';
-            rover.move(command);
+        test("Rover does not move beyond the left edge of the plateau", () => {
+            rover.move('L');
             expect(rover.direction).toEqual('W');
+
+            for (let i = 0; i < plateau.maxX + 1; i++) {
+                rover.move('M');
+            }
+            expect(rover.position).toEqual({ x: 0, y: 2 });
         })
     });
-
-    describe('When Rover faces to the east', () => {
-        beforeEach(() => {
-            rover.direction = 'E';
-        });
-
-        test("Rover faces to the north after it turns left", () => {
-            const command: Command = 'L';
-            rover.move(command);
-            expect(rover.direction).toEqual('N');
-        })
-
-        test("Rover faces to the south after it turns right", () => {
-            const command: Command = 'R';
-            rover.move(command);
-            expect(rover.direction).toEqual('S');
-        })
-    });
-
-    test("Rover moves up when rover faces to the north", () => {
-        const command: Command = 'M';
-        rover.move(command);
-        expect(rover.position).toEqual({ x: 1, y: 3 });
-    })
-
-    test("Rover moves right when rover faces to the east", () => {
-        rover.move('R');
-        expect(rover.direction).toEqual('E');
-
-        const command: Command = 'M';
-        rover.move(command);
-        expect(rover.position).toEqual({ x: 2, y: 2 });
-    })
-
-    test("Rover moves down when rover faces to the south", () => {
-        rover.move('R');
-        rover.move('R');
-        expect(rover.direction).toEqual('S');
-
-        const command: Command = 'M';
-        rover.move(command);
-        expect(rover.position).toEqual({ x: 1, y: 1 });
-    })
-
-    test("Rover moves left when rover faces to the west", () => {
-        rover.move('L');
-        expect(rover.direction).toEqual('W');
-
-        const command: Command = 'M';
-        rover.move(command);
-        expect(rover.position).toEqual({ x: 0, y: 2 });
-    })
-
-    test("Rover does not move beyond the upper edge of the plateau", () => {
-        for (let i = 0; i < plateau.maxY + 1; i++) {
-            rover.move('M');
-        }
-        expect(rover.position).toEqual({ x: 1, y: 5 });
-    })
-
-    test("Rover does not move beyond the bottom edge of the plateau", () => {
-        rover.move('R');
-        rover.move('R');
-        expect(rover.direction).toEqual('S');
-
-        for (let i = 0; i < plateau.maxY + 1; i++) {
-            rover.move('M');
-        }
-        expect(rover.position).toEqual({ x: 1, y: 0 });
-    });
-
-    test("Rover does not move beyond the right edge of the plateau", () => {
-        rover.move('R');
-        expect(rover.direction).toEqual('E');
-
-        for (let i = 0; i < plateau.maxX + 1; i++) {
-            rover.move('M');
-        }
-        expect(rover.position).toEqual({ x: 5, y: 2 });
-    })
-
-    test("Rover does not move beyond the left edge of the plateau", () => {
-        rover.move('L');
-        expect(rover.direction).toEqual('W');
-
-        for (let i = 0; i < plateau.maxX + 1; i++) {
-            rover.move('M');
-        }
-        expect(rover.position).toEqual({ x: 0, y: 2 });
-    })
-
 });
 
 test("Example - Single Rover Pattern 1", () => {
